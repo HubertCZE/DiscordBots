e7964f2ff34f6e532265b5b29b52b024df173659
HubertCZE <dafikdaf2011@gmail.com>
2026-03-02 07:40:53 +0100

    Add WEB_CONFIG with server details and settings

---
+++ config.js
@@ -0,0 +1,27 @@
+const WEB_CONFIG = {
+    version: "2.1",
+    total_slots: 10,
+    reserved_slots: 2,
+    taken_slots: 3,
+    form_link: "https://docs.google.com/forms/d/e/1FAIpQLSd8ZdHiZScuRCrou5hvk4UXR33DgGM_2nsIDpQGKV_9DrDRLg/viewform",
+    servers: [
+        {
+            name: "Game Server",
+            image: "GameFoto.jpg",
+            description: "Hlavní komunitní server pro hraní a zábavu. Tady najdeš moje nejnovější boty v akci!",
+            link: "https://discord.gg/EjRdU2mY47",
+            button_active: 1,
+            reserved: 1,
+            taken: 2
+        },
+        {
+            name: "Testovací Server",
+            image: "Profilovka.jpg",
+            description: "Místo pro testování nových funkcí a experimentální boty.",
+            link: "#",
+            button_active: 0,
+            reserved: 1,
+            taken: 1
+        }
+    ]
+};
